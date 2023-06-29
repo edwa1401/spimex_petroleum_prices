f654a6234334aa4d51d63ff4545c6db2ee6fa8c6
Dmitry Efimov <edwa14011977@gmail.com>
2023-06-29 20:09:09 +0300

    correct omissions

--- webapp/spimex_parser.py
+++ webapp/spimex_parser.py
@@ -13,16 +13,16 @@ class Spimex_data:
     volume_rub: int
 
 
-def upload_file_from_spimex() -> BinaryIO:
+def download_file_from_spimex() -> BinaryIO:
 
     url = 'https://spimex.com/upload/reports/oil_xls/oil_xls_20230628162000.xls'
     response = requests.get(url)
-    with open('spimex.xls', 'wb') as output:
+    with open('.data/spimex.xls', 'wb') as output:
         output.write(response.content)
 
 
 def read_spimex_file() -> Any:
-    workbook = xlrd.open_workbook('spimex.xls')
+    workbook = xlrd.open_workbook('.data/spimex.xls')
     sheet = workbook.sheet_by_index(0)
     for row in range(sheet.nrows):
         print(sheet.row(row))
